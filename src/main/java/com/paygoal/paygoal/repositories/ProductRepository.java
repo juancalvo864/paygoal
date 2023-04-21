0f8269ff8c785c4f608d237851e1fca32f963c97
Tu nombre <Tu correo>
2023-04-21 20:23:39 -0300

    actualizacion , armado de paquetes , clases e interfaces

--- src/main/java/com/paygoal/paygoal/repositories/ProductRepository.java
+++ src/main/java/com/paygoal/paygoal/repositories/ProductRepository.java
@@ -2,7 +2,10 @@ package com.paygoal.paygoal.repositories;
 
 import com.paygoal.paygoal.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ProductRepository extends JpaRepository <Product,Long> {
+
 
 }
--- src/main/java/com/paygoal/paygoal/services/ProductService.java
+++ src/main/java/com/paygoal/paygoal/services/ProductService.java
@@ -1,4 +1,16 @@
 package com.paygoal.paygoal.services;
 
+import com.paygoal.paygoal.models.Product;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductService {
+    public void save(Product product);
+
+    public List<Product> findAll();
+
+    Optional<Product> findById(Long id);
+
+
 }
