f5d49b1fe83d79c307c7467384d23bb21dbb4372
Tu nombre <Tu correo>
2023-04-22 14:16:47 -0300

    servicios

--- src/main/java/com/paygoal/paygoal/PaygoalApplication.java
+++ src/main/java/com/paygoal/paygoal/PaygoalApplication.java
@@ -1,7 +1,11 @@
 package com.paygoal.paygoal;
 
+import com.paygoal.paygoal.models.Product;
+import com.paygoal.paygoal.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PaygoalApplication {
@@ -10,5 +14,18 @@ public class PaygoalApplication {
 		SpringApplication.run(PaygoalApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner initData(ProductRepository productRepository) {
+		return (args) -> {
 
+			Product vaso = new Product("Vaso","Vaso de plastico 500cc",12.00,50);
+			Product plato = new Product("Plato","Plato de carton descartable",10.00,100);
+			Product cubiertos = new Product("Cubiertos","Cubiertos descartables , alta resistencia",5.50,150);
+
+
+			productRepository.save(vaso);
+			productRepository.save(plato);
+			productRepository.save(cubiertos);
+		};
+	}
 }
